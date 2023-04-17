1908f77245f4e5ddfe4181f4d61a54671794b293
Wojciech <szmelczerczykwojciech@gmail.com>
2023-04-17 18:19:35 +0200

    incorrect credentials test case included

--- README.md
+++ README.md
@@ -7,9 +7,42 @@ Sample test cases for the [login form](http://uitestingplayground.com/sampleapp)
 ## Table of contents
 
 - [Techstack](#techstack)
+- [To run](#to-run)
 
 ## Techstack
 
 - `Cypress`
+- `TypeScript`
 
-## To run 
+## Test cases
+
+|                      Test case                       |
+| :--------------------------------------------------: |
+|   [`Successful login`](./docs/login/successful.md)   |
+| [`Unsuccessful login`](./docs/login/unsuccessful.md) |
+
+## To run
+
+Clone repository.
+
+```sh
+git clone https://github.com/wojciechszmelczerczyk/login-form-ui-tests.git
+```
+
+Navigate to project root folder.
+
+```sh
+cd ./login-form-ui-tests
+```
+
+Install dependencies.
+
+```sh
+npm i
+```
+
+Run tests.
+
+```sh
+npm run cypress:open
+```
--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,16 +1,30 @@
 import * as userCredentials from "../fixtures/credentials.json";
 
-const { username, password } = userCredentials;
+const [correctUser, incorrectUser] = userCredentials;
 
 describe("Login Form", () => {
   it("if provided user credentials are correct, prompt successful login message", () => {
     cy.visit("http://uitestingplayground.com/sampleapp");
+
+    const { username, password } = correctUser;
 
     cy.login(username, password);
 
     cy.get('label[id="loginstatus"]').should(
       "have.text",
       `Welcome, ${username}!`
+    );
+  });
+  it("if provided user credentials are incorrect, prompt error validation message", () => {
+    cy.visit("http://uitestingplayground.com/sampleapp");
+
+    const { username, password } = incorrectUser;
+
+    cy.login(username, password);
+
+    cy.get('label[id="loginstatus"]').should(
+      "have.text",
+      "Invalid username/password"
     );
   });
 });
