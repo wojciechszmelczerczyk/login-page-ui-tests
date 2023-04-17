af0af9ca93d7afc8bfe21cd1d94da9ec4b0ac54d
Wojciech <szmelczerczykwojciech@gmail.com>
2023-04-17 15:21:51 +0200

    arbitrary successful login test implemented

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,14 @@
-describe("template spec", () => {
-  it("passes", () => {
+import userCredentials from "../fixtures/credentials.json";
+
+const { username, password } = userCredentials;
+
+describe("Login Form", () => {
+  it("if provided user credentials are correct, log in successfully", () => {
     cy.visit("http://uitestingplayground.com/sampleapp");
+
+    cy.get('[name="UserName"]').type(username);
+    cy.get('[name="Password"]').type(password);
+
+    cy.contains("Log In").click();
   });
 });
